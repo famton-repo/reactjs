55791e52638bfd237784bc1b44e94062ddb6e6a9
sudha-muthiah <sudha.muthiah-famteam@outlook.com>
2026-05-05 09:36:08 +0530

    change coding

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Homepage from './Pages/Homepage/Homepage'
 import ProductDetails from './Pages/ProductDetails/ProductDetails'
 import Cart from './Components/Cart/Cart'
 import { Routes, Route } from 'react-router-dom'
-import Hero from './Components/Hero/Hero'
+
 const App = () => {
   return (
     <div>
