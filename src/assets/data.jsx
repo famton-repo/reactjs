85f994d467698787ff7fd821e5cd4afd618ae07c
dhamini-maheshwaran <dhamini.maheshwaran-famteam@outlook.com>
2026-05-09 17:18:21 +0530

    Created Basic E-commerce Website using React Js and Vite

---
+++ src/assets/data.jsx
@@ -0,0 +1,125 @@
+import cloth1 from './cloth2.jpg'
+import cloth2 from './cloth3.jpg'
+import cloth3 from './cloth4.jpg'
+import cloth4 from './cloth5.jpg'
+import cloth5 from './cloth6.jpg'
+import cloth6 from './cloth7.jpg'
+import cloth7 from './cloth8.jpg'
+import cloth8 from './cloth9.jpg'
+import cloth9 from './cloth10.jpg'
+import cloth10 from './cloth11.jpg'
+import cloth11 from './cloth12.jpg'
+import cloth12 from './cloth13.jpg'
+
+
+
+export const productData = [
+    {
+        id: 1,
+        image: cloth1,
+        title: 'Womens Basic Casual Tops',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+
+    },
+    {
+        id: 2,
+        image: cloth2,
+        title: 'Womens Basic Casual Tops',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+    {
+        id: 3,
+        image: cloth3,
+        title: 'Womens Basic Casual Tops',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+    {
+        id: 4,
+        image: cloth4,
+        title: 'Mens Casual Sleeve Shirts',
+        price: 45,
+        category: 'mens',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+    {
+        id: 5,
+        image: cloth5,
+        title: 'Mens Casual Sleeve Shirts',
+        price: 45,
+        category: 'mens',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+    {
+        id: 6,
+        image: cloth6,
+        title: 'Mens Casual Sleeve Shirts',
+        price: 45,
+        category: 'mens',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+    {
+        id: 7,
+        image: cloth7,
+        title: 'Womens Basic Casual Tops',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+    {
+        id: 8,
+        image: cloth8,
+        title: 'Toddler Kids 2 Piece Outfit',
+        price: 45,
+        category: 'kids',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+    {
+        id: 9,
+        image: cloth9,
+        title: 'Toddler Kids 2 Piece Outfit',
+        price: 45,
+        category: 'kids',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        id: 10,
+        image: cloth10,
+        title: 'Toddler Kids 2 Piece Outfit',
+        price: 45,
+        category: 'kids',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+    {
+        id: 11,
+        image: cloth11,
+        title: 'Toddler Kids 2 Piece Outfit',
+        price: 45,
+        category: 'kids',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+    {
+        id: 12,
+        image: cloth12,
+        title: 'Mens Casual Sleeve Shirts',
+        price: 45,
+        category: 'mens',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+]
